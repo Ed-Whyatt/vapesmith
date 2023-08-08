29c1159720a5d6b51d753dfd113aa3ec0c2b82de
Ed Whyatt <lizardxyz@googlemail.com>
2023-08-08 10:59:26 +0000

    Remove owl cdn

---
+++ assets/js/postload.js
@@ -0,0 +1,25 @@
+// Owl Carousel start function.
+// Docs: https://owlcarousel2.github.io/OwlCarousel2/docs/started-installation.html
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 2,
+//     nav: false,
+//     autoplay: true,
+//     autoplayTimeout:4000,
+//     autoplayHoverPause: true,
+//     dots: false,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 2
+//         },
+//         1000: {
+//             items: 3
+//         },
+//         1500: {
+//             items: 4
+//         }
+//     }
+// });
