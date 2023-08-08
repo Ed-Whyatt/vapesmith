5f84eb444709e5c3506208d07b35a79eb367ad04
Ed Whyatt <lizardxyz@googlemail.com>
2023-08-08 12:32:40 +0000

    Add svg for recyling and lolilty

--- assets/js/postload.js
+++ assets/js/postload.js
@@ -1,25 +1,31 @@
+/* jshint esversion: 11, jquery: true */
 // Owl Carousel start function.
 // Docs: https://owlcarousel2.github.io/OwlCarousel2/docs/started-installation.html
-// $('.owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 2,
-//     nav: false,
-//     autoplay: true,
-//     autoplayTimeout:4000,
-//     autoplayHoverPause: true,
-//     dots: false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 2
-//         },
-//         1000: {
-//             items: 3
-//         },
-//         1500: {
-//             items: 4
-//         }
-//     }
-// });
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 2,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout:4000,
+    autoplayHoverPause: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        },
+        1500: {
+            items: 4
+        }
+    }
+});
+
+// Owl Carousel start function.
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
